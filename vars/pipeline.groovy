3f2f2851c1aaaff4ae2fadcf3b19e7377a322357
Krisztian <szecsikr@gmail.com>
2022-04-17 22:05:32 +0200

    pipeline

---
+++ vars/pipeline.groovy
@@ -0,0 +1,61 @@
+def call (){
+
+    podTemplate(yaml: '''
+        apiVersion: v1
+        kind: Pod
+        spec:
+          containers:
+          - name: node
+            image: node:alpine
+            command:
+            - cat
+            tty: true
+          - name: kubeclient
+            image: szecsikr/kubeclient
+            command:
+            - cat	
+            tty: true
+          - name: docker
+            image: docker
+            command:
+            - cat
+            tty: true
+            volumeMounts:
+              - name: dockersock
+                mountPath: /var/run/docker.sock
+          volumes:
+          - name: dockersock
+            hostPath:
+              path: /var/run/docker.sock
+'''){
+
+        node(POD_LABEL){
+            
+            stage('Build react project'){
+                container('node'){
+                    sh 'npm install'
+                    sh 'CI=false npm run build'
+                }
+            }
+
+            stage('Build docker image'){
+                container('docker'){
+                    image = docker.build("szecsikr/workout-frontend")
+                }
+            }
+
+
+            stage('Publish image'){
+                container('docker'){
+                    withCredentials([usernameColonPassword(credentialsId: 'szecsikr-docker', variable: 'DOCKER')]) {
+                        docker.withRegistry('', 'szecsikr-docker'){
+                            image.push();
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+}
